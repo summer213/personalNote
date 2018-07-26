1fc9745c7989981cfadd2f6fbf09f2eb565a358c
zxd <xiadanzhang@e-dewin.com>
2018-07-26 19:47:13 +0800

    edit变为组件

--- src/views/main.vue
+++ src/views/main.vue
@@ -5,54 +5,42 @@
 <template>
   <div class="index">
     <el-container>
-      <el-header>
-        <el-menu
-          class="el-menu-demo"
-          mode="horizontal"
-          background-color="#2a303c"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-          <el-menu-item index="1">记录</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">日期归档</template>
-            <el-menu-item index="2-1">2015-1</el-menu-item>
-            <el-menu-item index="2-2">2015-2</el-menu-item>
-            <el-menu-item index="2-3">2015-3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
+        <el-header>
+            <el-menu
+            class="el-menu-demo"
+            mode="horizontal"
+            background-color="#2a303c"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-menu-item index="1">记录</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">日期归档</template>
+                <el-menu-item index="2-1">2015-1</el-menu-item>
+                <el-menu-item index="2-2">2015-2</el-menu-item>
+                <el-menu-item index="2-3">2015-3</el-menu-item>
+                <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
             </el-submenu>
-          </el-submenu>
-        </el-menu>
-      </el-header>
-      <!-- <el-main> -->
-            <!-- <quill-editor :content="content"
-                          :options="editorOption"
-                          @change="onEditorChange($event)">
-            </quill-editor> -->
-      <!-- </el-main> -->
-  <form method="post"> 
-    <textarea class='tinymce-textarea' id="tinymceEditer"></textarea>
-  </form>
+            </el-menu>
+        </el-header>
+        <el-card class="box-card" v-for="k in 5" :key="k">
+            <div slot="header" class="clearfix">
+                <span>撒大大</span>
+                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+                {{'列表内容 ' + o }}
+            </div>
+        </el-card>
     </el-container>
   </div>
 </template>
 
 <script>
-import tinymce from 'tinymce/tinymce'
-import 'tinymce/themes/modern/theme'
-import 'tinymce/plugins/image'
-import 'tinymce/plugins/link'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/table'
-import 'tinymce/plugins/lists'
-import 'tinymce/plugins/contextmenu'
-import 'tinymce/plugins/wordcount'
-import 'tinymce/plugins/colorpicker'
-import 'tinymce/plugins/textcolor'
-// import Editor from '@tinymce/tinymce-vue'
 export default {
     name: 'text-editor',
     data () {
@@ -61,58 +49,6 @@ export default {
         };
     },
     methods: {
-        init () {
-            this.$nextTick(() => {
-                let vm = this;
-                let height = document.body.offsetHeight - 300;
-                tinymce.init({
-                    selector: '#tinymceEditer',
-                    branding: false,
-                    elementpath: false,
-                    height: height,
-                    language: 'zh_CN.GB2312',
-                    language_url: '/static/tinymce/zh_CN.GB2312.js',
-                    // images_upload_url: '/api/upfile',
-                    images_upload_handler : this.images_upload_handler ,
-                    skin_url: '/static/tinymce/skins/lightgray',
-                    menubar: 'edit insert view format table tools',
-                    plugins: ['link lists image code table colorpicker textcolor wordcount contextmenu'
-                    ],
-                    toolbar1: ' newnote print preview | undo redo | insert | styleselect | forecolor backcolor bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image emoticons media codesample',
-                    autosave_interval: '20s',
-                    image_advtab: true,
-                    table_default_styles: {
-                        width: '100%',
-                        borderCollapse: 'collapse'
-                    },
-                    setup: function (editor) {
-                        editor.on('init', function (e) {
-                            vm.spinShow = false;
-                            if (localStorage.editorContent) {
-                                tinymce.get('tinymceEditer').setContent(localStorage.editorContent);
-                            }
-                        });
-                        editor.on('keydown', function (e) {
-                            localStorage.editorContent = tinymce.get('tinymceEditer').getContent();
-                        });
-                    }
-                });
-            });
-        },
-        images_upload_handler (blobInfo, success, failure) {
-            this.handleImgUpload(blobInfo, success, failure);
-        },
-        handleImgUpload (blobInfo, success, failure) {
-            // let formdata = new FormData()
-            // success(blobInfo.content.data.fileURI);
-            success('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532577823094&di=338f9f4093aee9278dfc25b6601584a3&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01e8a157f86d8ca84a0d304fcb9943.jpg%402o.jpg')
-            // formdata.set('upload_file', blobInfo.blob())
-            // axios.post('/api/upload', formdata).then(res => {
-            //     success(res.data.data.src)
-            // }).catch(res => {
-            //     failure('error')
-            // })
-        },
         getMenu() {
             this.request('getMenue', {}, true).then(res => {
                 console.log(res);
@@ -120,11 +56,7 @@ export default {
         }
     },
     mounted () {
-        this.init();
         this.getMenu();
-    },
-    destroyed () {
-        tinymce.get('tinymceEditer').destroy();
     }
 };
 </script>
